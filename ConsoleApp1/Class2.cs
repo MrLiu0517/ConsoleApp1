97b3dc16c52b93265b96cbb5195dc2bf5c552de1
LiuYin <liuyin70517@163.com>
2023-03-20 15:15:37 +0800

    方法重载
    Signed-off-by: LiuYin <liuyin70517@163.com>

---
+++ ConsoleApp1/Class2.cs
@@ -0,0 +1,14 @@
+namespace ConsoleApp1;
+
+public class Class2
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public double Add(double a, double b)
+    {
+        return a + b;
+    }
+}
